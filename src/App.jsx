29c356b6100c4f72e9bd8f05db859b372cac1f8d
aybiya <yanez.aybi@gmail.com>
2023-11-08 04:11:10 -0300

    modif estilo

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   const [nombre, setName] = useState('');
   const [edad, setAge] = useState('');
   const [mensaje, setMssg] = useState('');
-  
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -16,26 +15,11 @@ function App() {
   const handleAgeChange = (event) => {
     setAge(parseInt(event.target.value));
   };
-
-  const createMssg = (event) => {
-    event.preventDefault(); // Evitar que el formulario se envíe y recargue la página
-    if (nombre.trim() === '' || edad === '') {
-      // Verificar que los campos requeridos esten completos
-      setMssg('Por favor, completa todos los datos requeridos' );
-    } else {
-      if (edad < 18) {
-        setMssg(`Hola ${nombre}, eres muy joven para usar esta aplicación`);
-      } else {
-        setMssg(`Bienvenido ${nombre}, gracias por usar nuestra aplicación`);
-      }
-    }
-  };
-
  
   return (
     <section>
       <h1>Hola!</h1>
-      <form>
+      <article>
         <div>
         <label htmlFor="name">Nombre: </label>
         <input type="text" id="name" value={nombre} placeholder="Nombre" onChange={handleNameChange}/>
@@ -44,9 +28,8 @@ function App() {
         <label htmlFor="age">Edad: </label>
         <input type="number" id="age" value={edad} placeholder="Edad" onChange={handleAgeChange}/>
         </div>
-        <button onClick={createMssg}>Aceptar</button>
-      </form>
-        <WelcomeMessage message={mensaje} />
+        <WelcomeMessage message={mensaje} nombre={nombre} edad={edad} setMssg={setMssg} />
+      </article>
     </section>
   );
 }
--- src/WelcomeMssg.jsx
+++ src/WelcomeMssg.jsx
@@ -1,12 +1,22 @@
 const WelcomeMessage = (props) => {
-  const WelcomeMessg = (message) => {
-    props.setMssg(message);
-  }
-return (
-  <div>
-    <p>{props.message}</p>
-  </div>
-);
-}
+
+  const createMssg = () => {
+    if (!props.nombre || !props.edad) {
+      props.setMssg('Por favor, completa todos los datos requeridos');
+    } else if (props.edad < 18) {
+      props.setMssg(`Hola ${props.nombre}, eres muy joven para usar esta aplicación`);
+    } else {
+      props.setMssg(`Bienvenido ${props.nombre}, gracias por usar nuestra aplicación`);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={createMssg}>Aceptar</button>
+      <p>{props.message}</p>
+    </div>
+  );
+  
+};
 
 export default WelcomeMessage;
